235704102e4dd013ea44bb9d2cf9235a116ac249
MGlolenstine <mglolenstine@gmail.com>
2021-02-28 00:48:02 +0100

    Initial push.

---
+++ README.md
@@ -0,0 +1,19 @@
+# Lyrics_ovh
+
+This is a library that allows you to fetch lyrics to a song of your choosing.
+
+This crate uses a [lyrics.ovh](https://lyrics.ovh) API to fetch lyrics.
+
+The library only contains one async method.
+
+## Example
+
+```Rust
+use lyrics_ovh::get_lyrics;
+
+#[tokio::main]
+pub async fn main(){
+    let lyrics = get_lyrics("Popular Monster", "Falling in reverse").await;
+    println!("Lyrics to the song Popular monster by Falling in reverse: \n {}", lyrics);
+}
+```
---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct Lyrics {
+    lyrics: String,
+}
+
+/// Fetch lyrics for the specified song.
+///```rust
+/// #[tokio::main]
+/// pub async fn main(){
+///     let lyrics = get_lyrics("Popular Monster", "Falling in reverse").await;
+///     println!("Lyrics to the song Popular monster by Falling in reverse: \n {}", lyrics);
+/// }
+///```
+pub async fn get_lyrics(title: &str, author: &str) -> Result<String, Error> {
+    let resp = reqwest::get(&format!("https://api.lyrics.ovh/v1/{}/{}", author, title))
+        .await?
+        .json::<Lyrics>()
+        .await?;
+    Ok(resp.lyrics)
+}
+
+#[tokio::test]
+async fn test_fetch_lyrics() {
+    let lyrics = get_lyrics("popular monster", "Falling in Reverse").await;
+    assert!(lyrics.is_ok());
+    let lyrics = lyrics.unwrap();
+    assert!(lyrics.starts_with("Yeah"));
+    assert!(lyrics.contains("And my doctor tries to tell me that I'm going through a phase"));
+}
